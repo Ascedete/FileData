63c2a213b9e7ab8946735aa8c882a786fbcaeaa9
Ascedete <patrickschneider2017@outlook.de>
2022-03-27 15:22:33 +0200

    -

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="FileData",
-    version="0.1.2",
+    version="0.1.3",
     description="Process files and multiline strings with additional information such as current linenumber",
     author="PTS",
     packages=["filedata"],
